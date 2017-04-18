f4b79de4b707bf2ab66ff669c187d4607e129161
StefanoGeezer <stefano.geezer@gmail.com>
2017-04-18 21:28:30 +0200

    Lista parchi definitiva

--- main/java/com/example/android/xyztouristattractions/provider/TouristAttractions.java
+++ main/java/com/example/android/xyztouristattractions/provider/TouristAttractions.java
@@ -55,20 +55,20 @@ public class TouristAttractions {
         put(CITY_TRENTO, new ArrayList<Attraction>() {{
             add(new Attraction(
                     "Parco naturale Paneveggio Pale di S.Martino", /*Titolo*/
-                    "Località Paneveggio 7, Predazzo TN, Trentino.", /*Indirizzo I scheda*/
+                    "Località Castelpietra 2, 38054 Tonadico (TN), Trentino.", /*Indirizzo I scheda*/
                     "Il territorio del Parco Naturale Paneveggio Pale di S. Martino è situato nelle Alpi Orientali (Dolomiti Trentino Orientale) e si sviluppa intorno ai bacini idrografici dei torrenti Cismon, Vanoi e Travignolo,coprendo la Val Venegia, la Foresta di Paneveggio, un'ampia porzione del Gruppo delle Pale di S.Martino, l'estremità orientale della catena del Lagorai ed una parte della catena Lusia - Cima bocche, aree che costituiscono Siti di Importanza Comunitaria e Zone di Protezione Speciale all'interno della Rete Europea \"Natura 2000\".",/*Descrizione*/
-                    "Sede del Parco Villa Welsperg\nTel. 0439 765973\n\nCentro Visitatori Paneveggio\nTel. 0462 575283\n\nCentro Visitatori S.Martino di Castrozza\nTel. 0439 768859\n\nCasa del Sentiero Etnografico del Vanoi\nTel. 0439 710049 (dal 1 luglio alla seconda domenica di settembre, negli altri periodi\nEcomuseo del Vanoi\nTel. 0439 719106",/*Numeri utilit*/
+                    "Sede del parco Villa Welsperg\nTel. 0439 765973\n\nCentro Visitatori Paneveggio\nTel. 0462 575283\n\nCentro Visitatori S.Martino di Castrozza\nTel. 0439 768859\n\nCasa del Sentiero Etnografico del Vanoi\nTel. 0439 710049 (dal 1 luglio alla seconda domenica di settembre, negli altri periodi\nEcomuseo del Vanoi\nTel. 0439 719106",/*Numeri utilit*/
                     Uri.parse("http://www.magicoveneto.it/Trentino/Paneveggio/C01-Parco-Naturale-Paneveggio-Pale-San-Martino.jpg"),/*Prima immagine*/
                     Uri.parse("http://www.aptcavalese.it/images/stories/mappe-trekking/lagorai.jpg"),/*Immagine mappa*/
-                    new LatLng(46.2785779,11.7797378), /*coordinate*/
+                    new LatLng(46.1991488,11.8663663), /*coordinate*/
                     CITY_TRENTO /*citta per ora solo trento*/
             ));
 
             add(new Attraction(
                     "Parco naturale provinciale dell'Adamello-Brenta",
-                    "Via Nazionale, 24 38080 Strembo (TN).",
+                    "Via Nazionale 24, 38080 Strembo (TN), Trentino.",
                     "Il Parco Naturale Adamello Brenta è la più vasta area protetta del Trentino, situato nel Trentino occidentale, con i suoi 620,51 kmq comprende i gruppi montuosi dell'Adamello e del Brenta, separati dalla Val Rendena e compresi tra le valli di Non, di Sole e Giudicarie. E' interessato dalla presenza di 48 laghi e dal ghiacciaio dell'Adamello, uno dei più estesi d'Europa.",
-                    "Sede del Parco Strembo\nTel. 0465 806666",
+                    "Sede del parco Strembo\nTel. 0465 806666",
                     Uri.parse("https://www.ufficiostampa.provincia.tn.it/var/002/storage/images/media/immagini-comunicati-stampa/le-lobbie-in-adamello-image/760878-1-ita-IT/Le-Lobbie-in-Adamello_imagefullwide.jpg"),/* Riga per foto. indirizzo da inserire tra virgolette. solo da internet */
                     Uri.parse("http://www.pnab.it/fileadmin/parco/Mobilità/sentieri/muoversi_a_piedi_h.jpg"),/** Riga per mappa. indirizzo da inserire tra virgolette. solo da internet */
                     new LatLng(46.2072398,10.7185963),
@@ -76,37 +76,42 @@ public class TouristAttractions {
             ));
 
             add(new Attraction(
-                    "Parco Naturale zonale della Maranza",
-                    "Rifugio Maranza, via cimirlo 1 TN, Trentino.",
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ut dui in ipsum suscipit aliquet pretium aliquet odio. Nam posuere nunc sed risus molestie varius. Suspendisse posuere faucibus urna, id vestibulum ante iaculis et. Vivamus placerat suscipit sem, a tempor nunc vehicula ac. Ut libero velit, dapibus sit amet euismod vel, dignissim a nisl.\n\nDonec non dui non felis laoreet malesuada. Fusce ac metus ultrices, fermentum felis quis, varius velit. Donec ac felis semper, scelerisque diam sed, dignissim risus. Maecenas vel semper sapien. Fusce euismod justo posuere, efficitur risus tincidunt, congue tellus. In hac habitasse platea dictumst. Sed lobortis risus consequat vehicula facilisis.\n\nIn hendrerit, neque in gravida rutrum, purus enim aliquet lectus, sit amet vulputate tortor lacus at sem. Aenean lorem metus, finibus rhoncus eros at, ullamcorper fringilla velit. Nulla vitae porttitor metus, quis gravida lectus. In rhoncus, diam a elementum luctus, erat nisi tempus ex, in porta est.",
-                    "Rifugio Maranza\nTel. 0461999999\n\nSoccorso Alpino\n1 1 8",
-                    Uri.parse("http://www.trentinotrailrunning.it/images/gallerie-articoli/marzola/IMG-037.jpg"),
-                    Uri.parse("http://1.bp.blogspot.com/-KjT5Zb7jZ0k/VFU0xWhyZfI/AAAAAAAAD9g/5YZnc8CBYnM/s1600/Marzola.JPG"),
-                    new LatLng(46.0449298,11.1419159),
+                    "Parco Naturale Monte Corno",
+                    "Via Am Kofl 2, 39040 Trodena (BZ), Sudtirolo.",
+                    "Il parco naturale Monte Corno è un'area naturale protetta in Sudtirolo (Alto Adige) di 6.660,00 ha, delimitata a nord dal passo di San Lugano, a sud-est dalla val di Fiemme, a ovest dalla valle dell'Adige e a sud con il confine provinciale con il Trentino.",
+                    "Sede del parco Trodena\nTel. 0471 869247",
+                    Uri.parse("http://www.suedtirolerland.it/images/cms/754x435/B-P1070241-Blaetterbachschlucht-Weisshorn.JPG"),
+                    Uri.parse("http://parchi-naturali.provincia.bz.it/images/map_MonteCorno.jpg"),
+                    new LatLng(46.3227165,11.3467833),
                     CITY_TRENTO
             ));
 
             add(new Attraction(
-                    "Parco nazionale della Majalla",
-                    "Via Badia 23, Sulmona AQ, Abruzzo.",
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et nunc in leo laoreet placerat. Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin vestibulum laoreet odio nec posuere. Quisque ante arcu, malesuada vitae velit a, auctor tincidunt ante. Mauris varius eros eros, eget scelerisque mi scelerisque ut. Donec vehicula vitae urna ac hendrerit. Phasellus egestas risus nec euismod auctor.\n\nInteger fermentum velit et dolor varius sagittis. Proin et viverra sapien. Nulla aliquet ante et hendrerit egestas. Duis vulputate libero in nisi gravida cursus. Praesent laoreet nec dolor non iaculis. Aliquam eleifend ultricies ipsum, eu pellentesque libero rutrum non. Mauris et purus erat. Nullam semper mi id tincidunt viverra. Ut porta sem congue lectus luctus ultricies. Suspendisse iaculis lacinia nibh, eu accumsan magna volutpat vel. Sed id interdum mi, vel sollicitudin elit. Fusce facilisis elementum gravida. Duis at volutpat odio. Integer porta convallis tincidunt. Donec aliquam, leo ut.",
-                    "Numeri",
-                    Uri.parse("https://lh4.googleusercontent.com/-wbNgVdUkBiE/VHe99hGVtNI/AAAAAAAAAFY/fAHfhchNLJw/w600-no/IMG_20141124_143747.jpg"),
-                    Uri.parse("https://lh6.googleusercontent.com/-sjY_xlEOic4/VHe9-I4DD9I/AAAAAAAAAFI/Mt0VnjU7SxQ/w600-no/IMG_20141124_144008.jpg"),
-                    new LatLng(46.0945482,11.130256),
+                    "Riserva regionale Tre Cime del Monte Bondone.",
+                    "Via Torre Verde 7, 38122 Trento (TN), Trentino.",
+                    "La Riserva naturale integrale delle Tre Cime del Monte Bondone è un'area naturale protetta del Trentino-Alto Adige istituita nel 1968. Occupa una superficie di 223,14 ha tra i 1580 e 2179 metri sul livello del mare nella Provincia Autonoma di Trento. " +
+                    "Il territorio della valle è di origine glaciale e raggiunge tre monti; questi sono, in ordine crescente di altezza: il Dos d'Abramo (2.140 m), il Cornetto (2.176 m) e la Cima Verde (2.102 m).",
+                    "A.P.T., Monte Bondone Valle dei Laghi\n Tel. 0461 216000",
+                    Uri.parse("http://www.lagodigardaescursioni.it/escursioni-lago-garda-montagna/Bondone-escursione/img-gr/02.jpg"),
+                    Uri.parse("https://comune.info/uploads/mappe-comune/cimone/satellite-strade-hibrida-cimone.png"),
+                    new LatLng(46.0703951,11.118943),
                     CITY_TRENTO
             ));
 
             add(new Attraction(
                     "Ecomuseo Argentario",
-                    "L'ecomuseo argentario . Aliquam eros velit, faucibus in mi in, accumsan eleifend magna. Fusce efficitur volutpat leo nec finibus. Vivamus luctus quis dolor ac interdum.",
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eros velit, faucibus in mi in, accumsan eleifend magna. Fusce efficitur volutpat leo nec finibus. Vivamus luctus quis dolor ac interdum. Donec iaculis, orci quis semper vulputate, tortor nisi porttitor tortor, at pretium ante quam ut odio. Donec fringilla sapien et dolor pharetra ultrices. Aenean faucibus felis non vulputate iaculis.\n\nEtiam eget dapibus ligula. Nunc facilisis dignissim tortor et elementum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam condimentum pellentesque mollis. Aliquam finibus urna ipsum, sed accumsan ante blandit quis. Vestibulum vel lacinia ligula. Nunc justo ex, volutpat nec justo ut, efficitur gravida lectus. Mauris cursus dui libero, vel tristique purus laoreet non.\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ultrices ullamcorper est, at consequat massa. Nam egestas at urna at pellentesque. Quisque lacus quam, efficitur vel erat eget, placerat feugiat eros. Mauris.",
-                    "Ass. sticavoli",
-                    Uri.parse("https://lh6.googleusercontent.com/-kypwDfnk674/VGLWpQPm4VI/AAAAAAAAAB0/SrfL0fE9DnE/w500-no/OI000020_2.jpg"),
-                    Uri.parse("https://lh3.googleusercontent.com/-6_Ioko2ysgU/VHva2PjmRCI/AAAAAAAAAGM/cHjJC7ney4Q/w500-no/PC190054.JPG"),
-                    new LatLng(46.0853217,11.1711999),
+                    "Via C. Battisti 1, 38045 Civezzano (TN), Trentino.",
+                    "L'Ecomuseo Argentario è un territorio ideale per lunghe passeggiate alla scoperta di luoghi nascosti. " +
+                            "È attraversato da una fitta rete di sentieri adatti a tutti: dai piccoli esploratori, " +
+                            "agli amanti della storia e della natura, agli sportivi. " +
+                            "Seguendo la segnaletica bianca e rossa potrete dare spazio alla vostra curiosità e voglia di avventura!",
+                    "Sede del parco Civezzano\nTel. 0461/858400, Cell. 3356514145",
+                    Uri.parse("https://www.visittrentino.info/website/var/tmp/image-thumbnails/110000/117027/thumb__contentgallery/ecomuseo-dell-argentario_2.jpeg"),
+                    Uri.parse("http://ecoargentario.it/media/files/PieghevoleCartina_EcomuseoArgentario_crocini.jpg"),
+                    new LatLng(46.0851318,11.1615498),
                     CITY_TRENTO
             ));
+
         }});
 
     }};
