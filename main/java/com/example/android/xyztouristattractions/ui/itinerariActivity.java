974177ecc95c8436e1077c42f529ce07028f28d0
Ale <alessio@MacBook-Pro-di-Alessio.local>
2017-04-19 02:36:14 +0200

    clean up

--- main/java/com/example/android/xyztouristattractions/ui/itinerariActivity.java
+++ main/java/com/example/android/xyztouristattractions/ui/itinerariActivity.java
@@ -1,6 +1,5 @@
 package com.example.android.xyztouristattractions.ui;
 
-import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
